84e75009c47065b3a664c42bb146350b8294d580
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-27 14:50:22 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,15 @@ import { Paginate } from '@/utils/Paginate'
 
 import { useEffect, useState } from 'react'
 
-export default function Home() {
+export default function Home({articles}) {
 
   const [posts, setPosts] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
   const pageSize = 10;
-
   useEffect(()=>{
-    // Get All the Articles Details 
-    const getPosts = async()=>{
-      const data= await fetch(
-        // 'https://jsonplaceholder.typicode.com/posts'
-        // 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
-        // 'https://newsapi.org/v2/everything?q=apple&from=2023-03-24&to=2023-03-26&sortBy=popularity&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
-        ).then(res=> res.json())
-      setPosts(data.articles.filter(article => article.urlToImage))
-    }
-    getPosts();
-  }, [])
+   setPosts(articles)
+
+  },[])
   
   const handlePageChange = page => setCurrentPage(page) ;
   const paginatePosts = Paginate(posts, currentPage, pageSize);
@@ -49,7 +39,7 @@ export default function Home() {
           }
         </div>
          <PageNumbers
-          items={posts.length}
+          items={paginatePosts.length}
           currentPage={currentPage}
           pageSize={pageSize}
           onPageChange={handlePageChange}
@@ -57,4 +47,17 @@ export default function Home() {
       </main>
     </>
   )
+}
+export const getServerSideProps =async ()=>{
+  const data= await fetch(
+    // 'https://jsonplaceholder.typicode.com/posts'
+    // 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
+    // 'https://newsapi.org/v2/everything?q=apple&from=2023-03-24&to=2023-03-26&sortBy=popularity&apiKey=d3d572148ab247a4b3dfe824bc3795d8'
+    ).then(res=> res.json())
+    return {
+      props:{
+        articles: data.articles.filter(article => article.urlToImage)
+      }
+    }
 }
